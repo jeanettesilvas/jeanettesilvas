2f4067d9dde227054b27855fb515750ba4b6e5d8
Jeanette Silvas <jeanette.a.silvas@gmail.com>
2018-07-01 21:38:39 -0700

    wip

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 
 import './Portfolio.css';
-import { Pin } from '../../icons';
+import { Code, Pin } from '../../icons';
 
 export class Portfolio extends Component {
     render() {
         return (
             <div className="portfolio">
                 <div className="portfolio__basics">
-                    <h1 className="portfolio__logo">{'Jeanette Silvas'}</h1>
+                    <h1 className="portfolio__logo">
+                        <Code className="portfolio__logo-icon" />
+                        {'Jeanette Silvas'}
+                    </h1>
                     <h2 className="portfolio__job-title">
                         {'frontend developer'}
                     </h2>
