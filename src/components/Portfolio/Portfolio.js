ca5cd08660c24c64261683a6fd8a329ffee5332d
Jeanette Silvas <jeanette.a.silvas@gmail.com>
2018-07-03 21:36:42 -0700

    Center header

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,29 +2,22 @@ import React, { Component } from 'react';
 
 import './Portfolio.css';
 import { Code, Pin } from '../../icons';
-// import homeaway from '../../images/homeaway.svg';
 
 export class Portfolio extends Component {
     render() {
         return (
             <div className="portfolio">
                 <div className="portfolio__basics">
-                    <h1 className="portfolio__logo">
-                        <Code className="portfolio__logo-icon" />
-                        {'Jeanette Silvas'}
-                    </h1>
+                    <h1 className="portfolio__logo">{'Jeanette Silvas'}</h1>
                     <h2 className="portfolio__job-title">
                         {'frontend developer'}
                     </h2>
-                    <h3 className="portfolio__location">
+                    <Code className="portfolio__logo-icon" />
+                    {/* <h3 className="portfolio__location">
                         <Pin className="portfolio__location-pin" />
-                        <span>{'Bothell, WA'}</span>
-                    </h3>
+                        {'Bothell, WA'}
+                    </h3> */}
                 </div>
-
-                {/* <div className="portfolio__current">
-                    <img src={homeaway} alt="" width="300" height="100" />
-                </div> */}
             </div>
         );
     }
