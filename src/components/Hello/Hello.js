3b2ac84bd97768ddcb25e5c6d65cad653951c097
Jeanette Silvas <jeanette.a.silvas@gmail.com>
2018-07-14 20:43:58 -0700

    Title animation change

--- src/components/Hello/Hello.js
+++ src/components/Hello/Hello.js
@@ -1,29 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classnames from 'classnames';
 
+import { getText } from '../../data/text';
 import './Hello.css';
 
-export function Hello() {
-    return (
-        <section className="hello">
-            <h2 className="hello__title">
-                <span className="hello__letter hello__h">{'h'}</span>
-                <span className="hello__letter hello__e">{'e'}</span>
-                <span className="hello__letter hello__l">{'l'}</span>
-                <span className="hello__letter hello__l-2">{'l'}</span>
-                <span className="hello__letter hello__o">{'o'}</span>
-                <span className="hello__letter hello__period">{'.'}</span>
-            </h2>
+export class Hello extends Component {
+    get title() {
+        let title = this.text.helloTitle.split('');
+        let delay = 0.5;
+        let style;
 
-            <p className="hello__p">
-                {
-                    'My web dev career started out writing Java but I quickly became focused on front end work. I love making interfaces that are enjoyable to use and look great. I’ve done some design work but I prefer working with great designers to help implement their vision.'
-                }
-            </p>
-            <p className="hello__p">
-                {
-                    'While I’ve worked with different languages and frameworks, I’m currently working with React both for work and for personal use. I spend a lot of my off time making goofy pop culture stuff out of yarn and have started two web projects I hope to reveal over the next year.'
-                }
-            </p>
-        </section>
-    );
+        let letters = title.map((letter, index) => {
+            let className = classnames('hello__letter', `hello__${index}`, {
+                hello__period: letter === '.',
+            });
+
+            delay = delay + 0.07;
+
+            style = {
+                animationDelay: `${delay}s`,
+            };
+
+            return (
+                <span
+                    style={style}
+                    key={`letter-${index}`}
+                    className={className}
+                >
+                    {letter}
+                </span>
+            );
+        });
+
+        return letters;
+    }
+
+    text = getText();
+
+    render() {
+        return (
+            <section className="hello">
+                <h2 className="hello__title">{this.title}</h2>
+
+                <p className="hello__p">
+                    {
+                        'My web dev career started out writing Java but I quickly became focused on front end work. I love making interfaces that are enjoyable to use and look great. I’ve done some design work but I prefer working with great designers to help implement their vision.'
+                    }
+                </p>
+                <p className="hello__p">
+                    {
+                        'While I’ve worked with different languages and frameworks, I’m currently working with React both for work and for personal use. I spend a lot of my off time making goofy pop culture stuff out of yarn and have started two web projects I hope to reveal over the next year.'
+                    }
+                </p>
+            </section>
+        );
+    }
 }
--- src/data/locales/en_US.js
+++ src/data/locales/en_US.js
@@ -1,5 +1,6 @@
 export let en_US = {
     currentJobTitle: 'front end developer',
+    helloTitle: 'hello.',
     maker: 'maker',
     name: 'Jeanette Silvas',
 };
